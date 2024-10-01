0572ffcfb3150e80e6ec6b1cf4b32e65d2d74901
johnnytran98765 <135786167+johnnytran98765@users.noreply.github.com>
2024-09-30 20:27:05 -0500

    Update server.js

--- Bruce Inventory System/Inventory Front-End/server.js
+++ Bruce Inventory System/Inventory Front-End/server.js
@@ -80,20 +80,6 @@ app.post('/add_beverage', function(req, res){
   
   });
 
-app.post('/update_beverage', function(req, res){
-    // create a variable to hold the floor num from the request body
-    var bevname = req.body.bevname
-    // create a variable to hold the new floor name from the request body
-    var newoh = req.body.newoh
-
-    axios.put('http://127.0.0.1:5000/api/beverages', {bev_name:bevname, bev_onhand:newoh})
-   console.log("Beverage: " + bevname);
-   console.log("Current OnHand: " + newoh);
-
-    res.redirect('/beverage')
-  
-  });
-
 app.post('/update_beverage/:bevname', function(req, res){
     var bevname = req.params.bevname;
     var newoh = req.body.newoh;
@@ -104,17 +90,6 @@ app.post('/update_beverage/:bevname', function(req, res){
 
     res.redirect('/beverage')
     });
-
-app.post('/delete_beverage', function(req, res){
-    // create a variable to hold the floor num from the request body
-    var bevname = req.body.bevname
-
-    axios.delete('http://127.0.0.1:5000/api/beverages', {data:{bev_name:bevname}})
-   console.log("bevname deleted is: " + bevname);
-
-    res.redirect('/beverage')
-
-  });
 
 app.get('/delete/:bevname', function(req, res){
     var bevname = req.params.bevname;
